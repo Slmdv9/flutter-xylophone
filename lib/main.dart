d9fedbded89df505cf501d1880d7c7e17944f1a3
Samuel <samuel9lmdv@gmail.com>
2024-02-29 16:44:53 +0100

    Add colors and notes for the XilophoneKeys

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const XylophoneApp());
@@ -10,8 +11,43 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SafeArea(child: Container()),
+        body: SafeArea(
+            child: Column(
+          children: const [
+            XylophoneKey(keyNumber: 1, color: Colors.red),
+            XylophoneKey(keyNumber: 2, color: Colors.orange),
+            XylophoneKey(keyNumber: 3, color: Colors.yellow),
+            XylophoneKey(keyNumber: 4, color: Colors.green),
+            XylophoneKey(keyNumber: 5, color: Colors.grey),
+            XylophoneKey(keyNumber: 6, color: Colors.blue),
+            XylophoneKey(keyNumber: 7, color: Colors.purple)
+          ],
+        )),
+      ),
+    );
+  }
+}
+
+class XylophoneKey extends StatelessWidget {
+  const XylophoneKey({super.key, required int keyNumber, required Color color})
+      : _keyNumber = keyNumber,
+        _color = color;
+
+  final int _keyNumber;
+  final Color _color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+        child: Container(color: _color),
+        onPressed: () async {
+          AudioCache.instance = AudioCache(prefix: '');
+          final player = AudioPlayer();
+          await player.play(AssetSource('assets/note$_keyNumber.wav'));
+        },
       ),
     );
   }
